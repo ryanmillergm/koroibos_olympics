0dfbcf04377bd6e47caae2087c946f761fc8b2c7
Ryan Miller <ryanmillergm@gmail.com>
2019-09-16 16:20:47 -0600

    Adds sports table to schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_221826) do
+ActiveRecord::Schema.define(version: 2019_09_16_221953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,12 @@ ActiveRecord::Schema.define(version: 2019_09_16_221826) do
     t.string "sex"
     t.integer "height"
     t.integer "weight"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sports", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
