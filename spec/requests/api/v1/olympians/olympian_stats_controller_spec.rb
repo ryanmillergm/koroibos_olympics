081f5127d2af296dffbd05f36683610a239d057a
Ryan Miller <ryanmillergm@gmail.com>
2019-09-17 10:03:44 -0600

    Adds get olympian stats test

---
+++ spec/requests/api/v1/olympians/olympian_stats_controller_spec.rb
@@ -0,0 +1,48 @@
+require "rails_helper"
+
+describe "olympian_stats API" do
+  before :each do
+    @olympian1 = Olympian.create(name: "John", team: "USA", age: 24, sex: "M", height: 170, weight: 180)
+    @olympian2 = Olympian.create(name: "Paul", team: "Canada", age: 36, sex: "M", height: 180, weight: 187)
+    @olympian3 = Olympian.create(name: "Sally", team: "Mexico", age: 19, sex: "F", height: 150, weight: 125)
+    @sport1 = Sport.create(name: "Gymnastics")
+    @sport2 = Sport.create(name: "Weights")
+    @game1 = Game.create(name: "2016 Winter Games")
+    @event1 = Event.create(name: "Gymnastics Men's Individual All-Around", sport_id: @sport1.id)
+    @event2 = Event.create(name: "Gymnastics Men's Individual One-one", sport_id: @sport2.id)
+    @oe1 = OlympianEvent.create(olympian_id: @olympian1.id, event_id: @event1.id, medal: "Silver")
+    @oe2 = OlympianEvent.create(olympian_id: @olympian2.id, event_id: @event1.id, medal: "Bronze")
+    @oe3 = OlympianEvent.create(olympian_id: @olympian3.id, event_id: @event1.id, medal: "Gold")
+    @oe4 = OlympianEvent.create(olympian_id: @olympian3.id, event_id: @event2.id, medal: "NA")
+    @gs1 = GameSport.create(olympian_id: @olympian1.id, sport_id: @sport1.id, game_id: @game1.id)
+    @gs2 = GameSport.create(game_id: @game1.id, sport_id: @sport1.id, olympian_id: @olympian2.id)
+    @gs3 = GameSport.create(game_id: @game1.id, sport_id: @sport1.id, olympian_id: @olympian3.id)
+    @gs4 = GameSport.create(game_id: @game1.id, sport_id: @sport2.id, olympian_id: @olympian3.id)
+  end
+
+  it "sends a list of olympians" do
+
+    get '/api/v1/olympian_stats'
+
+    expect(response).to be_successful
+
+    olympian_stats = JSON.parse(response.body)
+
+    binding.pry
+    attributes = ["name", "team", "age", "sport", "total_medals_won"]
+    expect = {
+                "unit": "kg",
+                "male_olympians": 75.4,
+                "female_olympians": 70.2
+              }
+
+    expect(olympian_stats.count).to eq(3)
+    expect(olympian_stats.keys).to eq(attributes)
+    expect(olympian_stats["total_competing_olympians"]).to be_a(Integer)
+    expect(olympian_stats["average_weight"]).to be_a(hash)
+    expect(olympian_stats["average_age"]).to be_a(Integer)
+    expect(olympian_stats["total_competing_olympians"]).to equal(3)
+    expect(olympian_stats["average_weight"]).to equal(expected)
+    expect(olympian_stats["average_age"]).to equal(28)
+  end
+end
